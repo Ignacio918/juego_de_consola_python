f3dfadb331cffe5c5c4f4ab6d6e2c85b6b9181eb
Ignacio <ignacampos@hotmail.com>
2024-10-28 17:47:55 -0300

    Juego de consola realizado en python de Dungeons And Dragons

---
+++ main.py
@@ -0,0 +1,73 @@
+import random
+from clases.enemigo import Enemigo
+from clases.jugador import Jugador
+
+
+def main():
+    nombre_jugador = input(
+        "Bienvenido a la aventura de Dungeons and Dragons! Por favor, ingresa tu nombre: "
+    )
+    jugador = Jugador(nombre_jugador)
+
+    enemigos = [
+        Enemigo("Goblin", 20, 10),
+        Enemigo("Zombie", 40, 20),
+        Enemigo("Hombre Lobo", 60, 40),
+    ]
+
+    enemigos_derrotados = []
+
+    print("¡Comienza la aventura!")
+
+    while enemigos:
+        enemigo_actual = random.choice(enemigos)
+        if enemigo_actual in enemigos_derrotados:
+            continue
+
+        print(f"Te encuentras conm un {enemigo_actual.nombre} en tu camino")
+
+        while enemigo_actual.salud > 0:
+            accion = input("¿Qué haces? (atacar/huir): ").lower()
+
+            if accion == "atacar":
+                dano_jugador = jugador.atacar()
+                print(
+                    f"Has atacado al {enemigo_actual.nombre} y le has causado {dano_jugador} de daño"
+                )
+                enemigo_actual.recibir_dano(dano_jugador)
+
+                if enemigo_actual.salud > 0:
+                    dano_enemigo = enemigo_actual.atacar()
+                    print(
+                        f"El {enemigo_actual.nombre} te atacó y te causó {dano_enemigo} de daño"
+                    )
+                    jugador.recibir_dano(dano_enemigo)
+
+            elif accion == "huir":
+                print("Cagón")
+                break
+
+        if jugador.salud <= 0:
+            print("Has perdido la partida")
+            break
+
+        if enemigo_actual.salud <= 0:
+            enemigos_derrotados.append(enemigo_actual)
+            enemigos.remove(enemigo_actual)
+
+        jugador.ganar_experiencia(20)
+
+        continuar = input("¿Quieres seguir explorando? (s/n): ").lower()
+
+        if continuar != "s":
+            print("¡Gracias por haber jugado Dungeons and Dragons!")
+            break
+
+    if not enemigos:
+        print("Felicidades has ganado la partida!")
+
+
+if (
+    __name__ == "__main__"
+):  # nos asegura que solo podremos ejecutar el script desde el programa principal
+    main()
